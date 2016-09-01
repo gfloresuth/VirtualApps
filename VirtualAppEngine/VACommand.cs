139cabe922c901156dc1df7153ef49227684e094
dexterminio <dexterminio@gmail.com>
2016-08-31 20:34:18 -0700

    VACommand and VAScope added

---
+++ VirtualAppEngine/VACommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VirtualAppEngine
+{
+    interface VACommand
+    {
+        void execute(VAScope scope);
+        void reportError(string title, string description, int value);
+    }
+}
---
+++ VirtualAppEngine/VAScope.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VirtualAppEngine
+{
+    class VAScope
+    {
+        private Dictionary<string, object> _values;
+        public void setValue(string name, object value)
+        {
+            if (existsValue(name)==false)
+            {
+                _values.Add(name, value);
+            }else
+            {
+                _values[name] = value;
+            }
+        }
+        public object getValue(string name)
+        {
+            return (existsValue(name) == true) ? _values[name] : null;
+        }
+        public Boolean existsValue(string name)
+        {
+            return _values.ContainsKey(name);
+        }
+    }
+}

6aa92c2cb6a6f0796b917b02290e35d6306b5b77
dexterminio <dexterminio@gmail.com>
2016-08-31 20:26:34 -0700

    Simple changes

--- VirtualApps/MainForm.Designer.cs
+++ VirtualApps/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VirtualApps
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,14 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(641, 301);
+            this.Name = "MainForm";
+            this.Text = "VirtualApps";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
